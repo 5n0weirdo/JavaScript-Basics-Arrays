067485b95fe0d1bfba4f682650dcd1234bebdf21
5n0weirdo <snoweirdo@protonmail.com>
2021-09-27 10:28:49 +0200

    Array Iteration

---
+++ 4-Array Iteration/01/index.js
@@ -0,0 +1,6 @@
+// for loop:
+var colors = ["red", "yellow", "blue"];
+
+for (var i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
---
+++ 4-Array Iteration/02/index.js
@@ -0,0 +1,8 @@
+// forEach:
+var colors = ["blue", "red", "black"];
+
+// arr.forEach(somefunction)
+colors.forEach(function(color) {
+  // color is a placeholder
+  console.log("Color: " + color);
+});
